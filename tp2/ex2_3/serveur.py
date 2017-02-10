eeea2953d74efbc05f4b8d810f067acc8a78bf26
Sheng SHEN <sshen@pc-df-310.priv.enst-bretagne.fr>
2017-02-10 14:56:15 +0100

    update tp2 ex2-3

--- tp2/ex2_3/serveur.py
+++ tp2/ex2_3/serveur.py
@@ -11,11 +11,14 @@ s.listen(10)
 print ("Serveur : %s:%s" %(HOST,PORT))
 print ("wait connexion")
 
+
+'''
 while True:
     conn,addr = s.accept()
     print 'connected by',addr
     def forum(conn):    
         while True:
+            print 'welcome %s' %addr
             data = conn.recv(1024)
             print '%s says: %s ' %(addr,data)
             reply = raw_input("reply un message:  ")
@@ -25,3 +28,23 @@ while True:
     t = threading.Thread(target = forum, args=[conn])
     t.start()
 s.close()
+'''
+
+while True:
+    connection,addr = s.accept()
+    connection2,addr = s.accept()
+    message = "begin"
+    while message != "finish":
+        message = connection.recv(1024)
+        print(message)
+        connection.send("you are first")
+        
+        message = connection2.recv(1024)
+        print (message)
+        connection2.send("you are second")
+        #message = connection.recv(1024)
+        
+    connection.send('Bye!')    
+    connection.close()
+    exit()	
+     
